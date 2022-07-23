fa480ade99d9fdc0b1a622f0be9279398714c935
Peter Tiedemann <pt@configit.com>
2022-07-24 00:12:34 +0200

    wip

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,67 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using LibGit2Sharp;
+﻿using LibGit2Sharp;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 
-Dictionary<AbsolutePath, HashSet<AbsolutePath>> CreateUsedByMap(Solution sln)
-{
-  HashSet<AbsolutePath> GetProjectReferences(Project p)
-  {
-    var project = p.GetMSBuildProject();
-    var paths = project.AllEvaluatedItems.Where(i => i.ItemType == "ProjectReference")
-      .Select(pr => p.Path.Parent / pr.EvaluatedInclude)
-      .ToArray();
-    return new HashSet<AbsolutePath>(paths);
-  }
+string rootRepoPath = args[0];
+string baseCommit = args[1];
 
-  {
-    var dictionary = sln.AllProjects.ToDictionary(p => p.Path, p => new HashSet<AbsolutePath>());
+var patches = GetPatches( rootRepoPath, baseCommit );
 
-    foreach (var project in sln.AllProjects)
-    {
-      var deps = GetProjectReferences(project);
-
-      foreach (var dep in deps)
-      {
-        dictionary[dep].Add(project.Path);
-      }
-    }
-
-    return dictionary;
-  }
+if ( patches.All( p => !p.Patch.Any() )) {
+  Console.WriteLine( "No changes at all ..." );
+  return 0;
 }
 
-var repo = new Repository(@"C:\git\core");
+var solution = ProjectModelTasks.ParseSolution(@"C:\git\mono-repo\mono.sln");
 
-var head = repo.Commits.First();
-var prev = repo.Commits.Skip(10).First();
+var changedFolders = patches
+  .SelectMany( p => p.Patch.Where( c => c.Mode != Mode.GitLink ).Select( f => (AbsolutePath) Path.Combine( p.WorkingDirectory, Path.GetDirectoryName( f.Path )! ) ) )
+  .Distinct()
+  .ToArray();
 
-// var patch = repo.Diff.Compare<Patch>(head.Tree, prev.Tree);
+var toAdd = new Stack<AbsolutePath>();
 
-var patch = repo.Diff.Compare<Patch>(head.Tree, DiffTargets.WorkingDirectory);
-
-var solution = ProjectModelTasks.ParseSolution(@"C:\git\core\Core.sln");
-
-var changedFolders = patch.Select(p => solution.Path!.Parent / Path.GetDirectoryName(p.Path)).Distinct().ToArray();
-
-var projectToUsers = CreateUsedByMap(solution);
-
-Stack<AbsolutePath> toAdd = new Stack<AbsolutePath>();
 foreach (var changedFolder in changedFolders)
 {
-  if (changedFolder == solution.Path!.Parent)
+  if (changedFolder == solution.Path!.Parent )
   {
     continue;
   }
 
+  // TODO: What do we want to do with changes to file not inside projects?
   foreach (var a in solution.AllProjects.Where(k => k.Path.Parent.Contains(changedFolder)))
   {
     toAdd.Push(a.Path);
   }
 }
 
-HashSet<AbsolutePath> affected = new HashSet<AbsolutePath>();
+var affected = new HashSet<AbsolutePath>();
+
+var projectToUsers = CreateUsedByMap(solution);
 
 while (toAdd.Count > 0)
 {
@@ -79,7 +55,68 @@ while (toAdd.Count > 0)
 }
 
 Console.WriteLine("Total affected:");
+
 foreach (var a in affected)
 {
   Console.WriteLine(a);
+}
+
+return 0;
+
+Dictionary<AbsolutePath, HashSet<AbsolutePath>> CreateUsedByMap( Solution sln ) {
+  HashSet<AbsolutePath> GetProjectReferences( Project p ) {
+    var project = p.GetMSBuildProject();
+    var paths = project.AllEvaluatedItems.Where( i => i.ItemType == "ProjectReference" )
+      .Select( pr => p.Path.Parent / pr.EvaluatedInclude )
+      .ToArray();
+    return new HashSet<AbsolutePath>( paths );
+  }
+
+  var dictionary = sln.AllProjects.ToDictionary( p => p.Path, p => new HashSet<AbsolutePath>() );
+
+  // TODO: We might also want to check for items with paths pointing above the project root
+  // e.g. a shared test binary file or similar
+
+  var projectDeps = sln.AllProjects.AsParallel().Select( p => ( project: p, usedBy: GetProjectReferences( p ) ) ).ToArray();
+
+  foreach ( var entry in projectDeps ) {
+    foreach ( var dep in entry.usedBy ) {
+      dictionary[dep].Add( entry.project.Path );
+    }
+  }
+
+  return dictionary;
+}
+
+static List<(string WorkingDirectory, Patch Patch)> GetPatches( string rootRepoPath, string rootBaseCommit ) {
+  var patches = new List<(string WorkingDirectory, Patch Patch)>();
+
+  var rootRepo = new Repository( rootRepoPath );
+
+  var rootPatch = rootRepo.Diff.Compare<Patch>(
+    rootRepo.Commits.Single( c => c.Sha == rootBaseCommit ).Tree,
+    rootRepo.Head.Tip.Tree );
+
+  patches.Add( ( rootRepoPath, rootPatch ) );
+
+  // This does not go recursive, but we don't have recursive submodules
+  foreach ( var gitLink in rootPatch.Where( f => f.Mode == Mode.GitLink ) ) {
+    var submodule = rootRepo.Submodules.FirstOrDefault( m => gitLink.Path == m.Path );
+    if ( submodule == null ) {
+      throw new InvalidOperationException( "Could not find submodule matching " + gitLink.Path );
+    }
+
+    var headOid = gitLink.Oid;
+    var baseOid = gitLink.OldOid;
+
+    var subRepoPath = Path.Combine( rootRepoPath, submodule.Path );
+    var subRepo = new Repository( subRepoPath );
+    var subPatch = subRepo.Diff.Compare<Patch>(
+      subRepo.Commits.Single( c => c.Sha == baseOid.Sha ).Tree,
+      subRepo.Commits.Single( c => c.Sha == headOid.Sha ).Tree );
+
+    patches.Add( ( subRepoPath, subPatch ) );
+  }
+
+  return patches;
 }
